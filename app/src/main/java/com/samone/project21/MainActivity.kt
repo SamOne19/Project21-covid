9377b0188c5e373baa90360a694ff68679516e63
Sameer <sameer0rathore@gmail.com>
2021-02-19 21:44:40 +0530

    mainActivity changes

--- app/src/main/java/com/samone/project21/MainActivity.kt
+++ app/src/main/java/com/samone/project21/MainActivity.kt
@@ -2,10 +2,116 @@ package com.samone.project21
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.AbsListView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stateListAdapter: StateListAdapter
+
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        list.addHeaderView(LayoutInflater.from(this).inflate(R.layout.list_header, list, false))
+
+        fetchResults()
+        swipeToRefresh.setOnRefreshListener {
+            fetchResults()
+        }
+        initWorker()
+        list.setOnScrollListener(object : AbsListView.OnScrollListener {
+            override fun onScrollStateChanged(view: AbsListView, scrollState: Int) {}
+            override fun onScroll(
+                view: AbsListView,
+                firstVisibleItem: Int,
+                visibleItemCount: Int,
+                totalItemCount: Int
+            ) {
+                if (list.getChildAt(0) != null) {
+                    swipeToRefresh.isEnabled = list.firstVisiblePosition === 0 && list.getChildAt(
+                        0
+                    ).getTop() === 0
+                }
+            }
+        })
+    }
+
+    private fun fetchResults() {
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO) { Client.api.clone().execute() }
+            if (response.isSuccessful) {
+                swipeToRefresh.isRefreshing = false
+                val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                    data.statewise?.get(0)?.let { bindCombinedData(it) }
+                    bindStateWiseData(data.statewise?.size?.let { data.statewise.subList(0, it) } as List<StatewiseItem>)
+                }
+            }
+        }
+    }
+
+    private fun bindStateWiseData(subList: List<StatewiseItem>) {
+        stateListAdapter = StateListAdapter(subList)
+        list.adapter = stateListAdapter
+    }
+
+    private fun bindCombinedData(data: StatewiseItem) {
+        val lastUpdatedTime = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(
+            simpleDateFormat.parse(lastUpdatedTime)
+        )}"
+
+        confirmedTv.text = data.confirmed
+        activeTv.text = data.active
+        recoveredTv.text = data.recovered
+        deceasedTv.text = data.deaths
+
+    }
+
+    @InternalCoroutinesApi
+    private fun initWorker() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val notificationWorkRequest =
+            PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.HOURS)
+                .setConstraints(constraints)
+                .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            "JOB_TAG",
+            ExistingPeriodicWorkPolicy.KEEP,
+            notificationWorkRequest
+        )
+    }
+
+}
+
+fun getTimeAgo(past: Date): String {
+    val now = Date()
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+    val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+    return when {
+        seconds < 60 -> {
+            "Few seconds ago"
+        }
+        minutes < 60 -> {
+            "$minutes minutes ago"
+        }
+        hours < 24 -> {
+            "$hours hour ${minutes % 60} min ago"
+        }
+        else -> {
+            SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
+        }
     }
 }
